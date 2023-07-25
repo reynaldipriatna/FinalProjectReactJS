6930b4d42b0acd7e39d2e54c7717f74cfbcd2866
Reynaldi Priatna <rey.twenty.one@gmail.com>
2023-07-25 17:33:27 +0700

    Update Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,28 @@
+import React from "react";
+import BannerCharacter from "../assets/images/banner-character.png";
+
 const Banner = () => {
   return (
-    <section className="flex h-[587px]">
-      <div>Banner</div>
+    <section className="flex h-[483px] mt-10 md:mt-0">
+      <div className="flex-1 flex justify-center items-start flex-col md:flex-row">
+        <div className="justify-center mt-[100px]">
+          <h2 className="banner-heading">
+            Jadi expert bersama, <br />
+            edspert.id
+          </h2>
+          <p className="banner-paragraph mt-5">
+            Tingkatkan skill dan pengetahuan bersama para mentor terbaik di
+            bidangnya, untuk siapkan karir impian anda.
+          </p>
+        </div>
+        <div>
+          <img
+            src={BannerCharacter}
+            alt="Banner Character"
+            className="h-[100%]"
+          />
+        </div>
+      </div>
     </section>
   );
 };
